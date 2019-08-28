9760738b8d59513bda122178c4ed22bb968b2226
Bruno Anderson Chen <mosoc@colle.co>
2019-08-29 01:36:19 +0900

    Change class name to container

--- src/App.tsx
+++ src/App.tsx
@@ -4,49 +4,49 @@ import './styles/index.scss';
 
 const orders = [
   {
-      name: 'Livi優活 抽取式衛生紙(100抽x10包x10串/箱)',
-      logo: 'https://static.oopocket.com/store/iconTreemall@3x.png',
-      status: {
-        code: 3,
-        type: '已取消'
-      },
-      date: '107/6/12'
+    name: 'Livi優活 抽取式衛生紙(100抽x10包x10串/箱)',
+    logo: 'https://static.oopocket.com/store/iconTreemall@3x.png',
+    status: {
+      code: 3,
+      type: '已取消'
+    },
+    date: '107/6/12'
   },
   {
-      name: 'BALMUDA The Toaster 百慕達烤麵包機-黑色',
-      logo: 'https://static.oopocket.com/store/iconTreemall@3x.png',
-      status: {
-        code: 2,
-        type: '已成立'
-      },
-      date: '108/7/21'
+    name: 'BALMUDA The Toaster 百慕達烤麵包機-黑色',
+    logo: 'https://static.oopocket.com/store/iconTreemall@3x.png',
+    status: {
+      code: 2,
+      type: '已成立'
+    },
+    date: '108/7/21'
   },
   {
-      name: '贈-短慧萬用鍋HD2133+三合一濾網「LG樂金」韓國原裝...',
-      logo: 'https://static.oopocket.com/store/iconTreemall@3x.png',
-      status: {
-        code: 1,
-        type: '處理中'
-      },
-      date: '108/6/2'
-   },
-   {
-      name: 'Apple AirPds 2',
-      logo: 'https://static.oopocket.com/store/iconTreemall@3x.png',
-      status: {
-        code: 4,
-        type: '已送達'
-      },
-      date: '108/3/02'
+    name: '贈-短慧萬用鍋HD2133+三合一濾網「LG樂金」韓國原裝...',
+    logo: 'https://static.oopocket.com/store/iconTreemall@3x.png',
+    status: {
+      code: 1,
+      type: '處理中'
+    },
+    date: '108/6/2'
+  },
+  {
+    name: 'Apple AirPds 2',
+    logo: 'https://static.oopocket.com/store/iconTreemall@3x.png',
+    status: {
+      code: 4,
+      type: '已送達'
+    },
+    date: '108/3/02'
   }
-]
+];
 
 const App: React.FC = () => {
   return (
-    <div className="App">
+    <div className="container">
       <OrderList orders={orders} />
     </div>
   );
-}
+};
 
 export default App;
