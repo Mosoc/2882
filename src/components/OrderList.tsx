49c95486e4352713013ad380bd334323ec6de528
Bruno Anderson Chen <mosoc@colle.co>
2019-08-29 09:50:57 +0900

    Finish  filter and sort

--- src/components/OrderList.tsx
+++ src/components/OrderList.tsx
@@ -6,19 +6,32 @@ import { Order, Orders, StatusCode } from '../types';
 const filterThenSort = (statusList: StatusCode[]) => (
   orders: Orders
 ): Orders => {
-
-  const filteredOrders = filter<Order>(order => statusList.includes(order.status.code), orders);
+  const filteredOrders = filter<Order>(
+    order => statusList.includes(order.status.code),
+    orders
+  );
   return sortWith<Order>([descend(prop('date'))])(filteredOrders);
 };
 
 const OrderList: React.FC<{ orders: Orders }> = ({ orders }) => {
   return (
     <div>
-      {filterThenSort([StatusCode.Processing, StatusCode.Confirmed])(
-        orders
-      ).map((item: Order) => (
-        <OrderItem key={item.toString()} {...item} />
-      ))}
+      <section>
+        <p>{`進行中`}</p>
+        {filterThenSort([StatusCode.Processing, StatusCode.Confirmed])(
+          orders
+        ).map((item: Order) => (
+          <OrderItem key={item.toString()} {...item} />
+        ))}
+      </section>
+      <section>
+      　<p>{`已完成`}</p>
+        {filterThenSort([StatusCode.Arrived, StatusCode.Cancelled])(
+          orders
+        ).map((item: Order) => (
+          <OrderItem key={item.toString()} {...item} />
+        ))}
+      </section>
     </div>
   );
 };
